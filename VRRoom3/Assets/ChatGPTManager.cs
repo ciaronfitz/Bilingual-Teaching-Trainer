867a3d5ff55481197a9e3846a251aa465fc17fc0
Ciaron Sean Fitzpatrick <ciaronf@me.com>
2024-06-21 20:05:38 +0100

    Update to allow personality parameters

--- VRRoom3/Assets/ChatGPTManager.cs
+++ VRRoom3/Assets/ChatGPTManager.cs
@@ -9,14 +9,72 @@ public class ChatGPTManager : MonoBehaviour
 {
     public OnResponseEvent OnResponse;
 
+    public List<NPCAction> actions;
+
+[System.Serializable]
+public struct NPCAction{
+        public string actionKeyword;
+       [TextArea(2,5)]
+        public string actionDescription;
+
+        public UnityEvent actionEvent;
+    }
+
+    [TextArea(5,20)]public string personality;
+    [TextArea(5,20)]public string scene;
+
+    public int maxResponseWordLimit = 15;
+
     [System.Serializable]
     public class OnResponseEvent : UnityEvent<string> { }
     private OpenAIApi openAI = new OpenAIApi();
     private List<ChatMessage> messages = new List<ChatMessage>();
 
+    public string getInstructions(){
+       string instructions =   "You are a video game character and will answer to the message the player ask you. \n" +
+
+            "You must reply to the player message only using the information from your Personnality and the Scene that are provided afterwards. \n" +
+
+            "Do not invent or create response that are not mentionned in these information. \n" +
+
+            "Do not break character or mention you are an AI or a video game character. \n" +
+
+
+
+            "You must answer in less than " + maxResponseWordLimit + "words. \n" +
+
+
+
+            "Here is the information about your personality : \n" +
+
+            personality + "\n" +
+
+
+
+            "Here is the information about the Scene around you : \n" +
+
+            scene + "\n" +
+
+            buildActionInstructions() + "\n" +
+
+            "Here is the message of the player : \n";
+
+return instructions;
+    }
+
+    public string buildActionInstructions(){
+        string intstructions = " ";
+
+        foreach (var item in actions){
+            intstructions += "if i imply that I want you to do the following : " + item.actionDescription + ". You must add to your answer the following keyword : " + item.actionKeyword + ". \n";
+        }
+
+        return intstructions;
+    }
+
     public async void AskChatGPT(string newText){
         ChatMessage newMessage = new ChatMessage();
-        newMessage.Content = newText;
+        newMessage.Content = getInstructions() + newText;
         newMessage.Role = "user";
 
         messages.Add(newMessage);
@@ -29,6 +87,17 @@ public class ChatGPTManager : MonoBehaviour
 
         if(response.Choices != null && response.Choices.Count > 0){
             var chatResponse = response.Choices[0].Message;
+
+            foreach (var item in actions)
+            {
+
+                if(chatResponse.Content.Contains(item.actionKeyword)){
+                    string textNoKeyword = chatResponse.Content.Replace(item.actionKeyword, "");
+                    chatResponse.Content = textNoKeyword;
+                    item.actionEvent.Invoke();
+                }
+            }
+
             messages.Add(chatResponse);
 
             Debug.Log(chatResponse.Content);
